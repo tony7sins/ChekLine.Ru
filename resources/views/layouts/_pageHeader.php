b14691dcad0e4e51d8c26885239c8ccb66daddee
t7sg <tonigalczeskij@gmail.com>
2018-09-26 02:29:22 +0300

    contacts view fixes

--- resources/views/layouts/_pageHeader.php
+++ resources/views/layouts/_pageHeader.php
@@ -1,8 +1,6 @@
-<section class="pageHeader" id="<?php echo $showcase; ?>">
-  <div class="container-fluid">
-    <div class="row">
-      <h1><?php echo $pageHeader; ?></h1><br>
-      <p><?php echo $pageDesc; ?></p>
-    </div>
+<div class="container-fluid">
+  <div class="row">
+    <h1><?php echo $pageHeader; ?></h1><br>
+    <p><?php echo $pageDesc; ?></p>
   </div>
-</section>
+</div>
