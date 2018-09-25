21d79e6cdf506fa850d669d2879d7013f164edc4
t7sg <tonigalczeskij@gmail.com>
2018-09-26 02:32:07 +0300

    little pageHeader fix

--- resources/views/layouts/_pageHeader.php
+++ resources/views/layouts/_pageHeader.php
@@ -1,6 +1,8 @@
-<div class="container-fluid">
-  <div class="row">
-    <h1><?php echo $pageHeader; ?></h1><br>
-    <p><?php echo $pageDesc; ?></p>
+<section class="pageHeader" id="<?php echo $showcase; ?>">
+  <div class="container-fluid">
+    <div class="row">
+      <h1><?php echo $pageHeader; ?></h1><br>
+      <p><?php echo $pageDesc; ?></p>
+    </div>
   </div>
-</div>
+</section>
